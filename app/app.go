abcf9a3e968e1cdabcaedf7bc0557179d2d311fc
melkor <melkor666free@gmail.com>
2019-03-28 11:12:20 +0100

    add help message and verbose/debug mode

--- app/app.go
+++ app/app.go
@@ -2,57 +2,90 @@ package app
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/melkor/fizzbuzz-rest/fizzbuzz"
+	log "github.com/sirupsen/logrus"
 )
 
 //App struct used to handle application
 type App struct {
-	router *mux.Router
+	address string
+	router  *mux.Router
 }
 
 //Init initialize application
-func New() *App {
-	a := &App{}
-	a.router = mux.NewRouter()
+func New(address string) *App {
+
+	// assume http.ListenAndServe check if address is valide
+	if address == "" {
+		address = ":8000"
+	}
+
+	a := &App{
+		address: address,
+		router:  mux.NewRouter(),
+	}
+
 	return a
 }
 
+//GetFizzBuzz is the endpoint behind the route "/fizzbuzz"
+// which will call fizzbuzz func and return result as a json message
 func (a *App) GetFizzBuzz(w http.ResponseWriter, r *http.Request) {
+	log.Debugln("received a fizzbuzz request")
+
 	w.Header().Set("Content-Type", "application/json")
 
 	params := mux.Vars(r)
 	paramsIntVal := make(map[string]int)
 
+	//convert into integer the parameters from querystring
+	// which must be handle as integer
 	for _, name := range []string{"int1", "int2", "limit"} {
 		val, err := strconv.Atoi(params[name])
+		log.Debugln("	param: ", name, ", value: ", val)
 		if err != nil {
-			w.WriteHeader(400)
-			log.Println("query error : ", err)
+			w.WriteHeader(http.StatusBadRequest)
+			log.Errorln("query error : ", err)
 			json.NewEncoder(w).Encode(map[string]string{"error": name + " must be an integer"})
 			return
 		}
 		paramsIntVal[name] = val
 	}
 
-	result, err := fizzbuzz.Fizzbuzz(paramsIntVal["int1"], paramsIntVal["int2"], paramsIntVal["limit"], params["str1"], params["str2"])
+	log.Debugln("	param: str1, value: ", params["str1"])
+	log.Debugln("	param: str1, value: ", params["str2"])
+
+	// call fizz-buzz function
+	result, err := fizzbuzz.Fizzbuzz(
+		paramsIntVal["int1"],
+		paramsIntVal["int2"],
+		paramsIntVal["limit"],
+		params["str1"],
+		params["str2"],
+	)
 
 	if err != nil {
-		w.WriteHeader(400)
-		log.Println("fizzbuzz error : ", err)
+		w.WriteHeader(http.StatusBadRequest)
+		log.Errorln("fizzbuzz error : ", err)
 		json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
 		return
 	}
+	log.Debugln("	result: ", result)
 
 	json.NewEncoder(w).Encode(result)
+	log.Debugln("Done...")
 }
 
+//add http route into this func
 func (a *App) initializeRoutes() {
-	//route "GET" to fizzbuzz
+	log.Infoln("Initialize routes")
+
+	//route "GET" to fizzbuzzaddr
+	log.Debugln("Add route to /fizzbuzz endpoint")
 	a.router.HandleFunc("/fizzbuzz", a.GetFizzBuzz).
 		Methods("GET").
 		Queries("int1", "{int1}").
@@ -60,10 +93,13 @@ func (a *App) initializeRoutes() {
 		Queries("limit", "{limit}").
 		Queries("str1", "{str1}").
 		Queries("str2", "{str2}")
+
+	//TODO add endpoint to stats
 }
 
 //Run function launch application
-func (a *App) Run(addr string) {
+func (a *App) Run() {
+	log.Infoln("Launch application")
 	a.initializeRoutes()
-	log.Fatal(http.ListenAndServe(addr, a.router))
+	log.Fatal(http.ListenAndServe(a.address, a.router))
 }
--- cmd/fizzbuzz-rest/main.go
+++ cmd/fizzbuzz-rest/main.go
@@ -1,10 +1,42 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/melkor/fizzbuzz-rest/app"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
 )
 
+var (
+	address = pflag.String("address", ":8000", "The listening address of the web server (default: ':8000')")
+	debug   = pflag.BoolP("debug", "d", false, "Activate debug mode")
+	help    = pflag.BoolP("help", "h", false, "Display usage message")
+	verbose = pflag.BoolP("verbose", "v", false, "Activate verbose mode")
+)
+
+// display help message and exit
+func displayHelp() {
+	fmt.Println(os.Args[0])
+	fmt.Println("Web server that will expose a REST API to execute fizz-buzz.")
+	pflag.PrintDefaults()
+}
+
 func main() {
-	app := app.New()
-	app.Run(":8000")
+	pflag.Parse()
+
+	if *help {
+		displayHelp()
+		os.Exit(0)
+	}
+
+	if *debug {
+		log.SetLevel(log.DebugLevel)
+	} else if *verbose {
+		log.SetLevel(log.InfoLevel)
+	}
+
+	app := app.New(*address)
+	app.Run()
 }
